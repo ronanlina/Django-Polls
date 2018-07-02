48ac72aa35bcf345a9c169243aac172a39e757fa
Ronan Lina <ronanlina@gmail.com>
2018-07-02 23:41:42 +0800

    database config, model migration, models.py

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+continue here
+
+"Playing with the API"
+
+https://docs.djangoproject.com/en/2.0/intro/tutorial02/
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# This is where we create database models (ORM)
+# Take notes later about migrating models.
+# Migrations are how Django stores changes to your models (and thus your database schema) - they’re just files on disk.
+# python manage.py migrate. migrates database models to sql databases.
+
+class Question(models.Model):
+    #table column           datatype    length/format/etc.
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0) 
